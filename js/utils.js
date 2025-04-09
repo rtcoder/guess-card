d9c447617f1b68a5f0dec7ad955a4900cad068da
Dawid Jeż <dawidjez@gmail.com>
2025-04-09 22:41:59 +0200

    Fix formatting in utility functions for consistency
    Aligned spacing in various utility function definitions to improve code readability. These changes are non-functional and solely enhance code style consistency.

--- js/utils.js
+++ js/utils.js
@@ -3,20 +3,21 @@ export async function sleep(ms) {
 }
 
 export function hide(...els) {
-    els.forEach(el=>el.setAttribute('hidden', ''));
+    els.forEach(el => el.setAttribute('hidden', ''));
 }
 
 export function show(...els) {
-    els.forEach(el=>el.removeAttribute('hidden'));
+    els.forEach(el => el.removeAttribute('hidden'));
 }
 
 export function getCardDiv(idx, card) {
     return `<div class="card" style="--i: ${idx}"><img src="./img/${card}.png" alt=""/></div>`;
 }
 
-export function getCardNames(cards){
+export function getCardNames(cards) {
     return cards.map(card => card.name);
 }
-export function shuffle(arr){
+
+export function shuffle(arr) {
     return arr.sort(() => Math.random() - 0.5);
 }
