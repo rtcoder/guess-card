ff8fb19dd0b8d6f924f8f7bc292d946afe6151dd
Dawid Jeż <dawidjez@gmail.com>
2025-04-09 23:25:59 +0200

    Refactor card deck generation logic
    Replaced the hardcoded card list with a dynamic generation approach using loops, simplifying maintenance and reducing redundancy. This change ensures consistency and makes it easier to adapt to future modifications in card properties.

--- js/consts.js
+++ js/consts.js
@@ -1,14 +1,14 @@
-export const allCards = [
-    '2c', '2d', '2h', '2s', '3c', '3d', '3h', '3s',
-    '4c', '4d', '4h', '4s', '5c', '5d', '5h', '5s',
-    '6c', '6d', '6h', '6s', '7c', '7d', '7h', '7s',
-    '8c', '8d', '8h', '8s', '9c', '9d', '9h', '9s',
-    '10c', '10d', '10h', '10s', 'ac', 'ad', 'ah', 'as',
-    'jc', 'jd', 'jh', 'js', 'kc', 'kd', 'kh', 'ks',
-    'qc', 'qd', 'qh', 'qs',
-];
+const figures = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'a', 'j', 'k', 'q'];
+const colors = ['c', 'd', 'h', 's'];
+const allCards = [];
 
-export const GAME = {
+figures.forEach(f => {
+    colors.forEach(c => {
+        allCards.push(`${f}${c}`);
+    });
+});
+
+const GAME = {
 
     selectedCards: [],
     cardMap: {},
@@ -24,3 +24,4 @@ export const GAME = {
 };
 
 
+export {allCards, GAME};
